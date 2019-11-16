abdd61da427048fd5edf2bdf8a6164979552f5a9
Ash <Ash@DESKTOP-0SM5GRD>
2019-11-15 20:42:34 -0500

    Added timer and points fixed shooter

--- BubbleShooter/src/Menus/EndGame.java
+++ BubbleShooter/src/Menus/EndGame.java
@@ -14,9 +14,11 @@ public class EndGame extends javax.swing.JFrame {
     /**
      * Creates new form EndGame
      */
-    public EndGame() {
+    public EndGame(int score) {
         initComponents();
+        jLabel2.setText(Integer.toString(score));
     }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -46,7 +48,7 @@ public class EndGame extends javax.swing.JFrame {
             .addGap(0, 100, Short.MAX_VALUE)
         );
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel2.setBackground(new java.awt.Color(0, 0, 51));
 
@@ -146,12 +148,12 @@ public class EndGame extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
+    /*
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -170,13 +172,13 @@ public class EndGame extends javax.swing.JFrame {
         }
         //</editor-fold>
 
-        /* Create and display the form */
+        Create and display the form
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new EndGame().setVisible(true);
+                new EndGame(0).setVisible(true);
             }
         });
-    }
+    }*/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
